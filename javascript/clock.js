3a582caf303b3a1dc569335361f04caec00c5417
Ruben Medina <dev@rubenmedina.com>
2017-11-28 02:44:27 -0800

    Clean up code a bit, add coloring to document background, change the way audio files are managed

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,28 +1,40 @@
-var pixelRatio = window.devicePixelRatio || 1;
 var canvas = document.getElementById("clock");
 var ctx = canvas.getContext("2d");
 var canvasDimensions;
 initCanvas();
 
-var animationPos=0;
-var xPos=0;
-var yPos=canvasDimensions.height;
-
-var date, hr, min, sec, time;
-var timeOfDay = 0;
-var clockPaused;
-var animationId;
-var animationSpeed = 90;
-var timeFontSize = 95;
-var bylineFontSize = 45;
-var bylineSpacing = 60;
+var clockPaused = false;
 var audioPlaying = false;
-var musicInitialized = false;
-var dayMusic;
-var nightMusic;
+var musicTracks = getMusicTracks();
+
+function getMusicTracks()
+{
+    var tracksObject = {};
+    var musicTracks = [
+        {
+            name: 'day',
+            filePath: 'sounds/unitock.mp3'
+        },
+        {
+            name: 'night',
+            filePath: 'sounds/unitocknight.mp3'
+        }
+    ];
+
+    musicTracks.forEach(function(track) {
+        tracksObject[track.name] = document.createElement('audio');
+        tracksObject[track.name].src = track.filePath;
+        tracksObject[track.name].preload = 'none';
+        tracksObject[track.name].loop = true;
+    });
+
+    return tracksObject;
+}
 
 function initCanvas()
 {
+    var pixelRatio = window.devicePixelRatio || 1;
+
     canvasDimensions = {
         width: window.innerWidth,
         height: window.innerHeight
@@ -42,16 +54,17 @@ function runClock()
 
 function updateScreen()
 {
-    if (audioPlaying && timeOfDay!==3)
-    {
-        nightMusic.pause();
-        dayMusic.play();
-    }
-    else if (audioPlaying && timeOfDay===3)
-    {
-        dayMusic.pause();
-        nightMusic.play();
-    }
+    var timeOfDay = 0;
+    var xPos = 0;
+    var yPos = canvasDimensions.height;
+    var animationPosition = 0;
+    var date, hr, min, sec;
+    var timeDisplay;
+    var animationSpeed = 90;
+    var timeFontSize = 95;
+    var bylineFontSize = 45;
+    var bylineSpacing = 60;
+    var displayProperties;
 
     date = new Date();
     hr = date.getHours();
@@ -61,28 +74,127 @@ function updateScreen()
     if (hr < 6 || hr > 22)
     {
         timeOfDay = 3;
-        animationPos = 4;
+        animationPosition = 4;
         xPos=0;
         yPos=0;
-        document.getElementById("music").style.backgroundColor = "#0C387C";
     }
     else if (hr < 11)
     {
         timeOfDay = 0;
-        document.getElementById("music").style.backgroundColor = "#568BDD";
     }
     else if (hr > 10 && hr < 18)
     {
         timeOfDay = 1;
-        document.getElementById("music").style.backgroundColor = "#145ECF";
     }
     else
     {
         timeOfDay = 2;
-        document.getElementById("music").style.backgroundColor = "#285EB0";
     }
 
-    if (hr===0)
+    if (audioPlaying)
+    {
+        if (timeOfDay === 3 && musicTracks.night.readyState >= 3)
+        {
+            musicTracks.day.pause();
+            musicTracks.night.play();
+        }
+        else if (musicTracks.day.readyState >= 3)
+        {
+            musicTracks.night.pause();
+            musicTracks.day.play();
+        }
+    }
+
+    displayProperties = getDisplayProperties(timeOfDay, animationPosition);
+
+    document.getElementById("music").style.backgroundColor = displayProperties.backgroundColor;
+    document.body.style.backgroundColor = displayProperties.backgroundColor;
+
+    timeDisplay = getTimeDisplay(hr, min, sec);
+
+    function moveScreen()
+    {
+        ctx.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height);
+
+        ctx.fillStyle = displayProperties.backgroundColor;
+        ctx.fillRect(xPos,yPos,canvasDimensions.width,canvasDimensions.height+animationSpeed);
+
+        ctx.fillStyle = displayProperties.textColor;
+
+        ctx.font = "bold " + timeFontSize + "px Arial";
+        ctx.textAlign = "center";
+        ctx.fillText(timeDisplay, canvasDimensions.width/2, canvasDimensions.height/2);
+        ctx.font = bylineFontSize + "px Arial";
+        ctx.fillText("samrum", canvasDimensions.width/2, (canvasDimensions.height/2)+bylineSpacing);
+
+        switch (animationPosition)
+        {
+            case 0:
+                yPos -= animationSpeed;
+
+                if (yPos <= -animationSpeed)
+                {
+                    xPos=0;
+                    yPos=0;
+                    animationPosition=1;
+                    return;
+                }
+                break;
+            case 1:
+                xPos += animationSpeed;
+
+                if (xPos > canvasDimensions.width+animationSpeed)
+                {
+                    xPos=0;
+                    yPos=-canvasDimensions.height;
+                    animationPosition=2;
+                    return;
+                }
+                break;
+            case 2:
+                yPos += animationSpeed;
+
+                if (yPos >= 0)
+                {
+                    xPos=0;
+                    yPos=0;
+                    animationPosition=3;
+                    return;
+                }
+                break;
+            case 3:
+                xPos -= animationSpeed;
+
+                if (xPos < -canvasDimensions.width-animationSpeed)
+                {
+                    xPos=0;
+                    yPos=canvasDimensions.height;
+                    animationPosition=0;
+                    return;
+                }
+                break;
+            case 4:
+                if (hr > 5 && (hr!==11 && hr!==12))
+                {
+                    animationPosition=1;
+                }
+                return;
+                break;
+            default:
+                animationPosition=0;
+        }
+
+        requestAnimationFrame(moveScreen);
+    }
+    if (!clockPaused)
+    {
+        requestAnimationFrame(moveScreen);
+    }
+}
+
+function getTimeDisplay(hr, min, sec)
+{
+    if (hr === 0)
     {
         hr = 12;
     }
@@ -101,172 +213,69 @@ function updateScreen()
         sec = "0" + sec;
     }
 
-    time = hr + " " + min + " " + sec;
+    return hr + " " + min + " " + sec;
+}
 
-    function moveScreen()
+function getDisplayProperties(timeOfDay, animationPosition)
+{
+    switch (timeOfDay)
     {
-        ctx.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height);
+        case 0:
+            backgroundColor = "#568BDD";
+            break;
+        case 1:
+            backgroundColor = "#145ECF";
+            break;
+        case 2:
+            backgroundColor = "#285EB0";
+            break;
+        case 3:
+            backgroundColor = "#0C387C";
+            break;
+        default:
+            backgroundColor = "#145ECF";
+    }
 
+    if (animationPosition === 0 || animationPosition === 2|| animationPosition === 4)
+    {
+        textColor = "#FFFFFF";
+    }
+    else
+    {
         switch (timeOfDay)
         {
-            case 0:
-                ctx.fillStyle = "#568BDD";
-                break;
             case 1:
-                ctx.fillStyle = "#145ECF";
+                textColor = "#145ECF";
                 break;
             case 2:
-                ctx.fillStyle = "#285EB0";
-                break;
-            case 3:
-                ctx.fillStyle = "#0C387C";
+                textColor = "#285EB8";
                 break;
             default:
-                ctx.fillStyle = "#145ECF";
+                textColor = "#568BDD";
         }
-
-        ctx.fillRect(xPos,yPos,canvasDimensions.width,canvasDimensions.height+animationSpeed);
-
-        if (animationPos===0||animationPos===2||animationPos===4)
-        {
-            ctx.fillStyle = "#FFFFFF";
-        }
-        else
-        {
-            switch (timeOfDay)
-            {
-                case 1:
-                    ctx.fillStyle = "#145ECF";
-                    break;
-                case 2:
-                    ctx.fillStyle = "#285EB8";
-                    break;
-                default:
-                    ctx.fillStyle = "#568BDD";
-            }
-        }
-
-        ctx.font = "bold " + timeFontSize + "px Arial";
-        ctx.textAlign = "center";
-        ctx.fillText(time, canvasDimensions.width/2, canvasDimensions.height/2);
-        ctx.font = bylineFontSize + "px Arial";
-        ctx.fillText("samrum", canvasDimensions.width/2, (canvasDimensions.height/2)+bylineSpacing);
-
-        switch (animationPos)
-        {
-            case 0:
-                yPos -= animationSpeed;
-
-                if (yPos <= -animationSpeed)
-                {
-                    xPos=0;
-                    yPos=0;
-                    animationPos=1;
-                    return;
-                }
-                break;
-            case 1:
-                xPos += animationSpeed;
-
-                if (xPos > canvasDimensions.width+animationSpeed)
-                {
-                    xPos=0;
-                    yPos=-canvasDimensions.height;
-                    animationPos=2;
-                    return;
-                }
-                break;
-            case 2:
-                yPos += animationSpeed;
-
-                if (yPos >= 0)
-                {
-                    xPos=0;
-                    yPos=0;
-                    animationPos=3;
-                    return;
-                }
-                break;
-            case 3:
-                xPos -= animationSpeed;
-
-                if (xPos < -canvasDimensions.width-animationSpeed)
-                {
-                    xPos=0;
-                    yPos=canvasDimensions.height;
-                    animationPos=0;
-                    return;
-                }
-                break;
-            case 4:
-                if (hr > 5 && (hr!==11 && hr!==12))
-                {
-                    animationPos=1;
-                }
-                return;
-                break;
-            default:
-                animationPos=0;
-        }
-
-        requestAnimationFrame(moveScreen);
     }
-    if (!clockPaused)
-    {
-        requestAnimationFrame(moveScreen);
+
+    return {
+        textColor: textColor,
+        backgroundColor: backgroundColor
     }
 }
 
 function toggleMusic()
 {
-    if (!musicInitialized)
-    {
-        dayMusic = new Audio("sounds/unitock.mp3");
-        nightMusic = new Audio("sounds/unitocknight.mp3");
-        dayMusic.loop = true;
-        nightMusic.loop = true;
-        musicInitialized = true;
-    }
-
-    if (audioPlaying)
-    {
-        if (timeOfDay!==3)
+    Object.keys(musicTracks).forEach(function(track) {
+        if (audioPlaying)
         {
-            dayMusic.pause();
-            dayMusic.currentTime = 0;
-            audioPlaying=false;
+            musicTracks[track].pause();
+            musicTracks[track].currentTime = 0;
         }
         else
         {
-            nightMusic.pause();
-            nightMusic.currentTime = 0;
-            audioPlaying=false;
+            musicTracks[track].preload = 'auto';
         }
-    }
-    else
-    {
-        if (isMobile())
-        {
-            if (timeOfDay!==3)
-            {
-                dayMusic.play();
-                dayMusic.pause();
-                dayMusic.currentTime = 0;
-            }
-            else
-            {
-                nightMusic.play();
-                nightMusic.pause();
-                nightMusic.currentTime = 0;
-            }
-        }
-        audioPlaying=true;
-    }
-}
+    });
 
-function isMobile()
-{
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    audioPlaying = !audioPlaying
 }
 
 window.onblur = function()
